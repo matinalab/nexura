c71f20c66365cc46e1a233cc5c4f8c7ee60f4c42
matinal <736408810@qq.com>
2026-06-09 17:43:00 +0800

    ai

--- packages/common/chat/index.ts
+++ packages/common/chat/index.ts
@@ -1,5 +1,5 @@
 export type ChatRole = 'human' | 'ai'; //角色 human: 用户 ai: 助手 langchain框架的枚举
-export type ChatRoleType = 'normal' | 'master' | 'business' | 'qilinge' | 'xiaoman'; //角色类型
+export type ChatRoleType = 'normal' | 'master' | 'kouhai' | 'matinal'; //角色类型
 export type ChatMessageType = 'reasoning' | 'chat'; //消息类型 reasoning: 推理 chat: 聊天
 //消息列表的对象类型
 export type ChatMessage = {
